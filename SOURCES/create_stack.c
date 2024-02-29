4a59e14d1320a7cfa827aa434bb7d0430a1b0aea
Clement Deville <cdeville@paul-f3Br4s6.clusters.42paris.fr>
2024-02-29 14:26:26 +0100

    Nothing changed

--- SOURCES/create_stack.c
+++ SOURCES/create_stack.c
@@ -6,7 +6,7 @@
 /*   By: cdeville <cdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 11:54:07 by cdeville          #+#    #+#             */
-/*   Updated: 2024/02/28 20:07:22 by cdeville         ###   ########.fr       */
+/*   Updated: 2024/02/29 14:25:42 by cdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,55 +51,55 @@ t_dblist	*create_stack(char *split_args[])
 }
 
 
-t_dblist	*add_number(char *arg, t_dblist **begin)
-{
-	int			*number;
+// t_dblist	*add_number(char *arg, t_dblist **begin)
+// {
+// 	int			*number;
 
-	number = ft_atoi_edited(arg);
-	if (!number)
-	{
-		clear_stacks(begin, NULL);
-		return (NULL);
-	}
-	if (!begin)
-		begin = ft_dblstfirst(number);
-	else
-		ft_dblstadd_before(begin, ft_dblstnew(number));
-	if (!begin->prev)
-	{
-		ft_free("%s", split_args);
-		exit_program(&begin, NULL);
-	}
-}
+// 	number = ft_atoi_edited(arg);
+// 	if (!number)
+// 	{
+// 		clear_stacks(begin, NULL);
+// 		return (NULL);
+// 	}
+// 	if (!begin)
+// 		begin = ft_dblstfirst(number);
+// 	else
+// 		ft_dblstadd_before(begin, ft_dblstnew(number));
+// 	if (!begin->prev)
+// 	{
+// 		ft_free("%s", split_args);
+// 		exit_program(&begin, NULL);
+// 	}
+// }
 
-t_dblist	*create_stack(char *split_args[])
-{
-	int			j;
-	int			*number;
-	t_dblist	*begin;
+// t_dblist	*create_stack(char *split_args[])
+// {
+// 	int			j;
+// 	int			*number;
+// 	t_dblist	*begin;
 
-	begin = NULL;
-	j = 0;
-	while (split_args[j])
-	{
-		number = ft_atoi_edited(split_args[j]);
-		if (!number)
-		{
-			ft_free("%s", split_args);
-			clear_stacks(&begin, NULL);
-			return (NULL);
-		}
-		if (!begin)
-			begin = ft_dblstfirst(number);
-		else
-			ft_dblstadd_before(begin, ft_dblstnew(number));
-		if (!begin->prev)
-		{
-			ft_free("%s", split_args);
-			exit_program(&begin, NULL);
-		}
-		j++;
-	}
-	ft_free("%s", split_args);
-	return (begin);
-}
+// 	begin = NULL;
+// 	j = 0;
+// 	while (split_args[j])
+// 	{
+// 		number = ft_atoi_edited(split_args[j]);
+// 		if (!number)
+// 		{
+// 			ft_free("%s", split_args);
+// 			clear_stacks(&begin, NULL);
+// 			return (NULL);
+// 		}
+// 		if (!begin)
+// 			begin = ft_dblstfirst(number);
+// 		else
+// 			ft_dblstadd_before(begin, ft_dblstnew(number));
+// 		if (!begin->prev)
+// 		{
+// 			ft_free("%s", split_args);
+// 			exit_program(&begin, NULL);
+// 		}
+// 		j++;
+// 	}
+// 	ft_free("%s", split_args);
+// 	return (begin);
+// }
